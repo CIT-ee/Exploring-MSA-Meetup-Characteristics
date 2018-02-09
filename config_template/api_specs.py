69a8d98981fc16a7906646eb5f568bc33e9a4639
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2018-02-09 16:50:53 +0000

    update: forgot to track the files inside config dir; fix a path

---
+++ config_template/api_specs.py
@@ -0,0 +1,25 @@
+props_for = { 
+    'locations': {
+        'City': 'city',
+        'Country': 'localized_country_name', 
+        'State': 'state', 
+        'ZipCode': 'zip', 
+        'Latitude': 'lat',
+        'Longitude': 'lon'
+    },
+    'groups': {
+        'only': [ 'id', 'name', 'status', 'link', 'created', 'lat', 'lon', 'who', 
+                'category.name', 'members', 'past_event_count', 'topics.urlkey' ],
+        'fields': [ 'past_event_count', 'topics' ]
+    },
+    'events':{
+        'topics': {
+            'only': [ 'id', 'time', 'group.category.id', 'yes_rsvp_count' ],
+            'fields': [ 'group_category' ]
+        },
+        'attendance': {
+            'only': [ 'id', 'time', 'venue.id', 'venue.zip', 'status', 'rsvp_limit', 'yes_rsvp' ],
+            'fields': [ ]
+        } 
+    }
+} 
---
+++ config_template/resources.py
@@ -0,0 +1,15 @@
+import os
+PATH_TO_PROJECT = os.environ['PROJECT_PATH']
+
+path_to = {
+    'chkpnts': os.path.join(os.environ['DATA_PATH'], 'raw', '{endpoint}', 'chkpnts'), 
+    'msa_codes': os.path.join(os.environ['DATA_PATH'], 'external', 'CBSA2015.csv'),
+    'cities': os.path.join(os.environ['DATA_PATH'], 'external', 'Cities_and_Towns_NTAD.csv'),
+    'meetup_locations': os.path.join(os.environ['DATA_PATH'], 'interim', 'locations', 'meetup_locations.csv'),
+    'meetup_locations_batch': os.path.join(os.environ['DATA_PATH'], 'interim', 'locations', 'batches', 'meetup_locations_{}.csv'),
+    'incorporated_places': os.path.join(os.environ['DATA_PATH'], 'external', 'Places-to-CBSA15_geocorr14.csv'),
+    'scraped_endpoint': os.path.join(os.environ['DATA_PATH'], 'raw', '{endpoint}', '{query}', 'meetup_{endpoint}.csv'),
+    'scraped_batch': os.path.join(os.environ['DATA_PATH'], 'raw', '{endpoint}', '{query}', 'batches', 'meetup_{endpoint}_{idx}.csv'),
+    'with_msa_endpoint': os.path.join(os.environ['DATA_PATH'], 'raw', '{endpoint}', '{query}', 'meetup_{endpoint}_with_msa.csv'),
+    'with_msa_batch': os.path.join(os.environ['DATA_PATH'], 'raw', '{endpoint}', '{query}', 'batches_with_msa', 'meetup_{endpoint}_{idx}_with_msa.csv'),
+}
